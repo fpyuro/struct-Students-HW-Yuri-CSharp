68d77b7bed5614e0a6916e47244dfa68f93b0aea
Fedor <Pyuro>
2020-12-02 14:22:09 +0600

    закончил

--- struct Students HW Yuri-CSharp/Program.cs
+++ struct Students HW Yuri-CSharp/Program.cs
@@ -4,8 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region условие
+//Создайте структуру с именем student, содержащую поля: фамилия и инициалы, номер группы, успеваемость (массив из пяти элементов).
+//Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла. 
+//Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
+#endregion
+
 namespace struct_Students_HW_Yuri_CSharp
 {
+    
     struct student
     {
         string SurnameIO; //фамилия и инициалы
@@ -17,9 +24,28 @@ namespace struct_Students_HW_Yuri_CSharp
             this.numberOfGroup = numberOfGroup;
             this.academicPerformance = academicPerformance;
         }
-        public string getSurnameIO()
+        public string getSurnameIO() => SurnameIO;
+        public int getNumberOfGroup() => numberOfGroup;
+        public double getAverageAcademicPerformance()
         {
-            return SurnameIO;
+            double sumOfRatings = 0; //сумма оценок
+            int numberOfRatings = 0; //количество оценок
+            for (int i = 0; i < academicPerformance.Length; ++i)
+            {
+                sumOfRatings += academicPerformance[i];
+                numberOfRatings++;
+            }
+            return sumOfRatings / numberOfRatings;
+        }
+        //проверка на отсутствие у студента двоек и троек
+        public bool check45()
+        {
+            bool check = true;
+            for(int i =0; i < academicPerformance.Length; ++i)
+            {
+                if (academicPerformance[i] < 4) check = false;
+            }
+            return check;
         }
     }
     class Students
@@ -33,6 +59,8 @@ namespace struct_Students_HW_Yuri_CSharp
                 this.Add();
             }
         } 
+        private static Random rand = new Random(); 
+        //метод заполнения всего массива студентов случайными студентами
         public void Add()
         {
             string[] name = { "Юрий", "Терентий", "Олег", "Никанор", "Матвей", "Арсений", "Никита", "Аскольд",
@@ -44,11 +72,9 @@ namespace struct_Students_HW_Yuri_CSharp
             string[] middlename = { "Матвеевич", "Георгиевич", "Измаилович", "Елисеевич", "Потапович", "Эрнестович",
                 "Моисеевич", "Мартьянович", "Никанорович", "Данилевич", "Прохорович", "Ипатиевич", "Феликсович",
                 "Дмитриевич", "Викентиевич", "Пахомович", "Семенович", "Измаилович", "Дмитриевич", "Герасимович" };
-            DateTime moment = DateTime.Now;
-            var rand = new Random((int)moment.Ticks);
             int newNumberOfGroup = rand.Next(3) + 1; //1-3
             string newSurnameIO = surname[rand.Next(surname.Length)] + " " + name[rand.Next(name.Length)][0] + "." + middlename[rand.Next(middlename.Length)][0] + ".";
-            int[] newAcademicPerformance = { rand.Next(5) + 1, rand.Next(5) + 1, rand.Next(5) + 1, rand.Next(5) + 1, rand.Next(5) + 1 };
+            int[] newAcademicPerformance = { rand.Next(4) + 2, rand.Next(4) + 2, rand.Next(4) + 2, rand.Next(4) + 2, rand.Next(4) + 2 }; //случайные оценки от 2 до 5 
             for (int i = 0; i < stdnts.Length; ++i)
             {
                 if (stdnts[i].getSurnameIO() == null)
@@ -58,20 +84,45 @@ namespace struct_Students_HW_Yuri_CSharp
                 }
             }
         }
-        public void Show()
+        //показывает весь массив студентов
+        public void ShowAll()
         {
             for(int i = 0; i< stdnts.Length; ++i)
-                Console.WriteLine(stdnts[i].getSurnameIO());
+                Console.WriteLine(stdnts[i].getSurnameIO() + "\tСредний балл студента: " + stdnts[i].getAverageAcademicPerformance());
+            Console.WriteLine();
         }
-
+        //сортировка по средней успеваемости
+        public void sortByMiddleAcademicPerformance()
+        {
+            double[] averagePerformance = new double[stdnts.Length];
+            for(int i =0; i < stdnts.Length; ++i)
+            {
+                averagePerformance[i] = stdnts[i].getAverageAcademicPerformance();
+            }
+            Array.Sort(averagePerformance, stdnts);
+        }
+        //показывает студентов отличников и ударников
+        public void Show45()
+        {
+            Console.WriteLine("Студенты имеющие четверки и пятерки:\n");
+            for(int i =0; i<stdnts.Length; ++i)
+            {
+                if(stdnts[i].check45())
+                    Console.WriteLine("Группа номер: " + stdnts[i].getNumberOfGroup() + " - " + stdnts[i].getSurnameIO());
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Students group1 = new Students(3);
-
-            group1.Show();
+            Students group1 = new Students(100); //по условию 10, но на 100 элементов больше шансов случайного создания ударников и отличников
+            Console.WriteLine("\nОбщий не сортированный список студентов:\n");
+            group1.ShowAll();
+            group1.sortByMiddleAcademicPerformance();
+            Console.WriteLine("Общий отсортированный список студентов:\n");
+            group1.ShowAll();
+            group1.Show45();
             Console.ReadLine();
         }
     }
